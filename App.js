27723c305e47517bfed5655dd71e86e0cd9108e3
chidiebereojingwa <ojingwaoji@gmail.com>
2022-02-14 15:52:10 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import InCallManager from 'react-native-incall-manager'
 
 export default function App() {
+
+  InCallManager.start({media: 'audio'}); // audio/video, default: audio
+
+  InCallManager.stop();
+
+  DeviceEventEmitter.addListener('WiredHeadset', function (data) {
+    // --- do something with events
+})
+
+      async getIsWiredHeadsetPluggedIn() {
+        if (Platform.OS === 'ios') {
+            return await InCallManager.getIsWiredHeadsetPluggedIn();
+        } else {
+            console.log("Android doesn't support getIsWiredHeadsetPluggedIn() yet.");
+            return null;
+        }
+      }
+      async getAudioUri(audioType, fileType) {
+        if (typeof this.audioUriMap[audioType] === "undefined") {
+            return null;
+        }
+        if (this.audioUriMap[audioType][fileType]) {
+            return this.audioUriMap[audioType][fileType];
+        } else {
+            try {
+                let result = await InCallManager.getAudioUriJS(audioType, fileType);
+                if (typeof result === 'string' && result.length > 0) {
+                    this.audioUriMap[audioType][fileType] = result;
+                    return result
+                } else {
+                    return null;
+                }
+            } catch (err) {
+                return null;
+            }
+        }
+      }
+
+    async chooseAudioRoute(route) {
+      let result = await InCallManager.chooseAudioRoute(route);
+      return result;
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
