424675d0366662dfb84d214db031bda32374aa3b
chidiebereojingwa <ojingwaoji@gmail.com>
2022-02-14 17:37:12 +0100

    updated app.jss commit

--- App.js
+++ App.js
@@ -1,64 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import InCallManager from 'react-native-incall-manager'
+import { useState, useEffect } from 'react'
+import HeadphoneDetection from 'react-native-headphone-detection'
 
-export default function App() {
+export default function useHeadphone() {
+  const [isHeadphoneConnected, setIsHeadphoneConnected] = useState<
+    boolean | undefined
+  >(false)
 
-  InCallManager.start({media: 'audio'}); // audio/video, default: audio
-
-  InCallManager.stop();
-
-  DeviceEventEmitter.addListener('WiredHeadset', function (data) {
-    // --- do something with events
-})
-
-      async getIsWiredHeadsetPluggedIn() {
-        if (Platform.OS === 'ios') {
-            return await InCallManager.getIsWiredHeadsetPluggedIn();
-        } else {
-            console.log("Android doesn't support getIsWiredHeadsetPluggedIn() yet.");
-            return null;
-        }
-      }
-      async getAudioUri(audioType, fileType) {
-        if (typeof this.audioUriMap[audioType] === "undefined") {
-            return null;
-        }
-        if (this.audioUriMap[audioType][fileType]) {
-            return this.audioUriMap[audioType][fileType];
-        } else {
-            try {
-                let result = await InCallManager.getAudioUriJS(audioType, fileType);
-                if (typeof result === 'string' && result.length > 0) {
-                    this.audioUriMap[audioType][fileType] = result;
-                    return result
-                } else {
-                    return null;
-                }
-            } catch (err) {
-                return null;
-            }
-        }
-      }
-
-    async chooseAudioRoute(route) {
-      let result = await InCallManager.chooseAudioRoute(route);
-      return result;
+  useEffect(() => {
+    HeadphoneDetection.addListener((result) => {
+      setIsHeadphoneConnected(result.audioJack || result.bluetooth)
+    })
+    return () => {
+      //@ts-ignore
+      if (HeadphoneDetection.remove) HeadphoneDetection.remove()
     }
+  }, [])
 
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return { isHeadphoneConnected }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
